4d9693586617e996206e1bd0dfe48778f4638b24
RBSIVAKALISANKARAN <rbsiva421688@gmail.com>
2026-09-13 20:54:57 +0530

    Paginate enhanced money locations

--- wallet/location_features.py
+++ wallet/location_features.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import MoneyLocation
+from .pagination import StandardResultsSetPagination
 
 
 @api_view(['GET'])
@@ -16,4 +17,7 @@ def enhanced_money_locations(request):
     for name, location_type in defaults:
         MoneyLocation.objects.get_or_create(name=name, defaults={'location_type': location_type, 'active': True})
     locations = MoneyLocation.objects.filter(active=True).order_by('name')
-    return Response([{'id': str(x.id), 'name': x.name, 'location_type': x.location_type, 'active': x.active} for x in locations])
+    paginator = StandardResultsSetPagination()
+    page = paginator.paginate_queryset(locations, request)
+    data = [{'id': str(x.id), 'name': x.name, 'location_type': x.location_type, 'active': x.active} for x in page]
+    return paginator.get_paginated_response(data)
